d842547400619532d97cf9d6398c8180e3651da8
Brian Langley <brian.langley@student.rocva.nl>
2022-04-19 10:46:41 +0200

    Form data is posted

--- SQL/queries.php
+++ SQL/queries.php
@@ -1,15 +1,29 @@
 <?php
-
 // Verbinding met de database
 include_once 'db_connect.php';
 
-// Standaard queries
+// form data to database
+if (isset($_POST['paste'])) {
+    $taal = $_POST['taal'];
+    $titel = $_POST['titel'];
+    $code = $_POST['code'];
+    $datum = $_POST['datum'];
+    $wachtwoord = $_POST['waachtwoord'];
+    $url = $_POST['url'];
 
-
-// Specifiek queries
-
-
-// Button clicks
-
+    $sqlUpdate = "INSERT INTO movies
+    SET `taal` = '$taal',
+    `titel` = '$titel',
+    `code` = '$code',
+    `datum` = '$datum',
+    `wachtwoord` = '$wachtwoord',
+    `url` = '$url';";
+    $update = $db->query($sqlUpdate);
+    if ($update) {
+        echo '<script>alert("Paste is geplaatst!")</script>';
+    } else {
+        echo '<script>alert("Paste is niet geplaatst!")</script>';
+    }
+}
 
 ?>
--- index.php
+++ index.php
@@ -2,11 +2,39 @@
 <?php
 // Includes
 include_once './SQL/db_connect.php';
-// include_once './SQL/queries.php';
 include './PHP/functions/url_generator.php';
-// Url generator
+include './PHP/functions/datum.php';
+
+// Variables
 $url = generateUrl();
 $password = generateUrl();
+$datum = datum();
+
+// form data to database
+if (isset($_POST['taal'])) {
+    $taal = $_POST['taal'];
+    $titel = $_POST['titel'];
+    $code = $_POST['code'];
+    $datum = $_POST['datum'];
+    $wachtwoord = $_POST['wachtwoord'];
+    $url = $_POST['url'];
+
+    $sqlUpdate = "INSERT INTO posts
+    SET `taal` = '$taal',
+    `titel` = '$titel',
+    `code` = '$code',
+    `datum` = '$datum',
+    `wachtwoord` = '$wachtwoord',
+    `url` = '$url';";
+    $update = $db->query($sqlUpdate);
+    if ($update) {
+        echo '<script>alert("Paste is geplaatst!")</script>';
+    } else {
+        echo '<script>alert("Paste is niet geplaatst!")</script>';
+    }
+}
+
+
 
 ?>
 
@@ -30,14 +58,18 @@ $password = generateUrl();
 
 <body>
     <nav class="font-sans flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-white shadow sm:items-baseline w-full">
+
         <div class="mb-2 sm:mb-0 flex flex-row">
+
             <div class="h-10 w-10 self-center mr-2">
                 <img class="h-10 w-10 self-center" src="">
             </div>
+
             <div>
-                <a href="/" class="text-2xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold">PasteBit</a><br>
+                <a href="./index.php" class="text-2xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold">PasteBit</a><br>
                 <span class="text-xs text-grey-dark">Delen doe je hier!</span>
             </div>
+
         </div>
 
         <div class="sm:mb-0 self-center">
@@ -45,8 +77,8 @@ $password = generateUrl();
             <a href="#" class="text-md no-underline text-grey-darker hover:text-blue-dark ml-2 px-1 hover:bg-gray-200 transition duration-500 hover:scale-125">Public</a>
             <a href="./PHP/project.php" class="text-md no-underline text-black hover:text-blue-dark ml-2 px-1 hover:bg-gray-200 transition duration-500 hover:scale-125">Project PasteBit</a>
             <a href="./PHP/legaal.php" class="text-md no-underline text-black hover:text-blue-dark ml-2 px-1 hover:bg-gray-200 transition duration-500 hover:scale-125">Licenties</a>
-
         </div>
+
     </nav>
 
 
@@ -58,45 +90,42 @@ $password = generateUrl();
         </article>
 
         <article id="landing-form">
-            <form method="POST" id="pasteBinPost">
-                <textarea class="pasteBinText" placeholder="Typ of plak hier uw code"></textarea>
+            <form method="POST" id="pasteBinPost" action="index.php">
+
+                <textarea id="code" name="code" class="pasteBinText" placeholder="Typ of plak hier uw code"></textarea>
+
+                <div class="mt-16 mb-16 w-full flex ml-10">
+
+                    <div class="float-left h-7">
+                        <h4 class="mt-2.5 font-bold leading-5 text-base mb-2.5">Welke taal?</h4>
+                        <h4 class="mt-7 text-base mb-2.5 leading-5 font-bold">Titel</h4>
+                        <h4 class="mt-7 text-base mb-2.5 leading-5 font-bold" name="wachtwoord">Wachtwoord</h4>
+                        <p class="mb-2.5"> </p>
+                    </div>
+
+                    <div class="block float-right box-border pl-5 flex flex-col">
+
+                        <select class="h-10 border-solid border-2 border-gray-500" name="taal" required>
+                            <option value="">Selecteer een optie</option>
+                            <option value="HTML">HTML</option>
+                            <option value="CSS">CSS</option>
+                            <option value="PHP">PHP</option>
+                            <option value="SQL">MySQL</option>
+                        </select>
+
+                        <input type="text" name="titel" placeholder="Geef een titel aan" class="mt-5 text-base mb-2.5 leading-5 font-bold" required>
+                        <input type="text" name="wachtwoord" value="<?= $password ?>" placeholder="Vul een wachtwoord in" class="mt-7 text-base leading-5 font-bold" id="">
+                        <input type="text" hidden name="url" value="<?= $url ?>">
+                        <input type="text" hidden name="datum" value="<?= $datum ?>">
+                        <input type="submit" value="paste" class="bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                    </div>
+
+                </div>
             </form>
-            <p>URL = <?= $url; ?> </p>
+
         </article>
+
     </section>
-
-    <!-- Subheading -->
-    <div class="mt-16 mb-16 w-full flex ml-10">
-        <!-- Left side -->
-        <div class="float-left h-7">
-            <h4 class="mt-2.5 font-bold leading-5 text-base mb-2.5">Syntax Hightlighting</h4>
-
-
-            <h4 class="mt-7 text-base mb-2.5 leading-5 font-bold">Password</h4>
-            <p class="mb-2.5"> </p>
-
-            <h4 class="mt-7 text-base mb-2.5 leading-5 font-bold">Titel paste</h4>
-
-            <button class="bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                Paste
-            </button>
-        </div>
-
-        <!-- Right side -->
-        <div class="block float-right box-border pl-5 flex flex-col">
-            <select class="h-10 border-solid border-2 border-gray-500">
-                <option value="">Selecteer een optie</option>
-                <option value="HTML">HTML</option>
-                <option value="CSS">CSS</option>
-                <option value="PHP">PHP</option>
-                <option value="MySQL">MySQL</option>
-            </select>
-
-            <input type="text" name="password" value="<?= $password ?>" placeholder="Vul een wachtwoord in" class="mt-7 text-base leading-5 font-bold" id="">
-
-            <input type="text" name="titel" placeholder="Geef een titel aan" class="mt-5 text-base mb-2.5 leading-5 font-bold" id="">
-        </div>
-    </div>
 
 </body>
 
